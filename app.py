bb757494e3d127e429a21ebd656ba7e356d4a595
PhuNong <32190177+phunong@users.noreply.github.com>
2017-11-17 01:48:01 +0700

    bankrate

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
-    if req.get("result").get("action") != "shipping.cost":
+    if req.get("result").get("action") != "bankrate":
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
